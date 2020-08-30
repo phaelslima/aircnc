2a1c1996727ebda87b4c08d737910e65b3911942
Raphael Lima <phaelslima@gmail.com>
2020-08-29 21:21:26 -0300

    feat(backend): config db

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv-flow').config();
+
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 
 app.use(express.json());
 app.use(routes);
