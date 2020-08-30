58792117fd94b51c7949a407cab214d5913a632d
Raphael Lima <phaelslima@gmail.com>
2020-08-30 11:03:49 -0300

    feat(backend): add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 require('dotenv-flow').config();
 
@@ -15,6 +16,7 @@ mongoose.connect(
   },
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
