2d7c001b1b91c755eccc7c484b2243a1e58cfd7e
Raphael Lima <phaelslima@gmail.com>
2020-08-30 12:51:20 -0300

    feat(frontend): add pages and routes (Login, Dashboard, New)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import './App.css';
+import logo from './assets/logo.svg';
+
+import Routes from './routes';
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+
+      <div className="content">
+        <Routes />
+      </div>
+    </div>
+  );
 }
 
 export default App;
