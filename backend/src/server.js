89c592533fa83da25c8065c47df637d5df18ed9a
Raphael Lima <phaelslima@gmail.com>
2020-09-01 19:39:26 -0300

    feat(backend): add socket

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,16 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 require('dotenv-flow').config();
 
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
@@ -17,9 +22,24 @@ mongoose.connect(
   },
 );
 
+const connectedUsers = {};
+
+io.on('connection', (socket) => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(process.env.PORT);
+server.listen(process.env.PORT);
